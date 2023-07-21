f7b69820e212bf00921885d5f808cf0edede3dfb
TKoNG056 <125735932+TKoNG056@users.noreply.github.com>
2023-07-21 20:11:27 +0700

    Add files via upload

---
+++ lab3q4_65056.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int sum=0;
+	int count=0;
+	int input;
+	printf("enter number:");
+	while(1){
+		scanf("%d",&input);
+		if(input<=0){
+			break;
+		}
+		count++;
+		sum+=input;
+	}
+	float avg = (float)sum/count;
+	printf("Summation=%d\n",sum);
+	printf("Mean=%.2f\n",avg);
+	return 0;
+}
